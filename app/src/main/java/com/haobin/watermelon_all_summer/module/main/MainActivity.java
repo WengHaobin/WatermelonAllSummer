8c4a979704801bd1409ef05bf3f8ca0c51ac2bc9
Wenghaobin <532602716@qq.com>
2018-10-12 18:03:34 +0800

    1. 着手开始写首页的UI

--- app/src/main/java/com/haobin/watermelon_all_summer/module/main/MainActivity.java
+++ app/src/main/java/com/haobin/watermelon_all_summer/module/main/MainActivity.java
@@ -1,7 +1,17 @@
 package com.haobin.watermelon_all_summer.module.main;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 
+import com.haobin.watermelon_all_summer.R;
+
+import butterknife.BindView;
 import cn.droidlover.xdroidmvp.mvp.XActivity;
 
 /**
@@ -11,6 +21,17 @@ import cn.droidlover.xdroidmvp.mvp.XActivity;
  */
 public class MainActivity extends XActivity {
 
+    @BindView(R.id.fl_title_menu)
+    FrameLayout flTitleMenu;
+    @BindView(R.id.toolbar_main)
+    Toolbar toolbarMain;
+    @BindView(R.id.vp_content)
+    ViewPager vpContent;
+    @BindView(R.id.nv_main)
+    NavigationView nvMain;
+    @BindView(R.id.dl_main)
+    DrawerLayout dlMain;
+
     @Override
     public void initData(Bundle savedInstanceState) {
 
@@ -18,11 +39,37 @@ public class MainActivity extends XActivity {
 
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.activity_main;
     }
 
     @Override
     public Object newP() {
         return null;
+    }
+
+    @Override
+    public void bindEvent() {
+        super.bindEvent();
+        initToolBar();
+        initNavigationView();
+    }
+
+    private void initToolBar(){
+        // 设置titleBar
+        setSupportActionBar(toolbarMain);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //去除默认Title显示
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowHomeEnabled(false);
+        }
+    }
+
+    private void initNavigationView(){
+        // 联系titleBar 和 navigationView
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, dlMain, toolbarMain, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+        dlMain.addDrawerListener(toggle);
     }
 }
